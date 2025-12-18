d34b5b6b99c8ab50db3d8daac2a6091820fdbfa5
elleehk <elleehk@hotmail.com>
2025-12-18 18:04:41 +0800

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,15 +1,4 @@
 def update_dictionary(dct, key, value):
-    """
-    Task 1
-    - Create a function that updates a dictionary (dct) with a new key-value pair.
-    - If the key already exists in dct, print the original value, then update its value.
-    - Return the updated dictionary.
-    """
-    return
-
-Answer:
-
-def update_dictionary(dct, key, value):
     # Check if dct is a dictionary
     if not type(dct).__name__ == 'dict':
         return -1
@@ -29,9 +18,5 @@ def update_dictionary(dct, key, value):
 # - {}, "name", "Alice"
 # - {"age": 25}, "age", 26
 
-Answer:
-
 print(update_dictionary({}, "name", "Alice"))
-
 print(update_dictionary({"age": 25}, "age", 26))
-
